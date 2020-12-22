cdcccd764e023114d5dcb24fcfa9f4c8589e3d09
Thin Buffalo <mcluozc@163.com>
2020-12-22 13:38:52 +0800

    P1055

--- luogu/P1055.cpp
+++ luogu/P1055.cpp
@@ -1,15 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-char isbn[13];
-int num[11];
+char isbn[14], c[13] = "0123456789X";
+int n = 1, sum;
 int main()
 {
-    scanf("%s",isbn);
-    for(int i=0;i<13;i++)
+    scanf("%s", isbn);
+    for (int i = 0; i < 12; i++)
     {
-        if(isbn[i]!='-')
+        if (isbn[i] == '-')
         {
-            num[i+1]=
+            continue; 
         }
+        n++;
+        sum += (isbn[i] - 48) * n;
     }
+    sum %= 11;
+    if (sum == (isbn[13] - 48))
+    {
+        printf("Right");
+    }
+    else
+    {
+        isbn[13] = c[sum];
+        printf("%s", isbn);
+    }
+    system("pause");
+    return 0;
 }
