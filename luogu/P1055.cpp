dfbb8461a983c0bb334d0e0b9d62fc4f739c3363
Thin Buffalo <mcluozc@163.com>
2020-12-22 13:43:52 +0800

    P1055

--- luogu/P1055.cpp
+++ luogu/P1055.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-char isbn[14], c[13] = "0123456789X";
+char isbn[14], c[12] = "0123456789X";
 int n = 1, sum;
 int main()
 {
@@ -11,17 +11,17 @@ int main()
         {
             continue; 
         }
-        n++;
         sum += (isbn[i] - 48) * n;
+        n++;
     }
     sum %= 11;
-    if (sum == (isbn[13] - 48))
+    if (c[sum] ==isbn[12])
     {
         printf("Right");
     }
     else
     {
-        isbn[13] = c[sum];
+        isbn[12] = c[sum];
         printf("%s", isbn);
     }
     system("pause");
