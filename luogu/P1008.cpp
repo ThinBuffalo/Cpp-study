f4f6fcfead3c2953930b0857fd1eb55e0abb0b65
Thin Buffalo <mcluozc@163.com>
2020-12-23 17:34:58 +0800

    2020/12/23

--- luogu/P1008.cpp
+++ luogu/P1008.cpp
@@ -1,43 +1,40 @@
 #include <bits/stdc++.h>
+#include<stdio.h>
 using namespace std;
 bool num[10];
+bool repeat(int n)
+{
+    int a=n/100;
+    int b=n%100/10;
+    int c=n%10;
+    if(num[a]||num[b]||num[c]||a==b||a==c||a==b||a==0||b==0||c==0)
+    {
+        return 0;
+    }
+    else
+    {
+        num[a]=num[b]=num[c]=1;
+        return 1;
+    }
+}
 int main()
 {
-    for (int i = 123; i <= 987; i++)
+    for(int i=123;i<=333;i++)
     {
-        num[i / 100] = 1;
-        num[i % 100 / 10] = 1;
-        num[i % 10] = 1;
-        for (int j = i + 1; j <= 987; j++)
+        if(repeat(i))
         {
-            if (num[j / 100] || num[j % 100 / 10] || num[j % 10])
+            int j=i*2;
+            if(repeat(j))
             {
-                continue;
-            }
-            else
-            {
-                num[j / 100] = 1;
-                num[j % 100 / 10] = 1;
-                num[j % 10] = 1;
-            }
-            for (int k = j + 1; k <= 987; k++)
-            {
-                if (num[k / 100] || num[k % 100 / 10] || num[k % 10])
+                int k=i*3;
+                if(repeat(k))
                 {
-                    continue;
-                }
-                if((j / i) == 2 && (k / i) == 3)
-                {
-                    printf("%d %d %d\n",i,j,k);
+                    printf("%d %d %d\n", i, j, k);
                 }
             }
-            num[j / 100] = 0;
-            num[j % 100 / 10] = 0;
-            num[j % 10] = 0;
+            num[j/100]=num[j%100/10]=num[j%10]=0;
         }
-        num[i / 100] = 0;
-        num[i % 100 / 10] = 0;
-        num[i % 10] = 0;
+        num[i/100]=num[i%100/10]=num[i%10]=0;
     }
     system("pause");
     return 0;
