660e2dcde9888b1d54ccbe85a9393a484dc6ee84
Thin Buffalo <mcluozc@163.com>
2021-01-04 13:25:21 +0800

    2021/1/4

--- ppt/Unit3/3-2-5.cpp
+++ ppt/Unit3/3-2-5.cpp
@@ -1,6 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    int x;
+    scanf("%d", &x);
+    if ((x % 4 == 0) && (x % 100 != 0) || (x % 400 == 0))
+    {
+        printf("%d是闰年", x);
+    }
+    else
+    {
+        printf("%d不是闰年", x);
+    }
+    system("pause");
+    return 0;
 }
---
+++ ppt/Unit3/3-3-1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if(n<=5)
+    {
+        printf("%d" ,300*n);
+    }
+    else
+    {
+        printf("%d" ,280*n);
+    }
+    system("pause");
+    return 0;
+}
