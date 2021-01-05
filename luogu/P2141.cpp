a50f66500a5ea36fb5972e08f83ef7e76573d167
Thin Buffalo <mcluozc@163.com>
2021-01-05 13:34:14 +0800

    2021/1/5

---
+++ luogu/P2141.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool flag[101];
+int num[101],n,t;
+int main()
+{
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        scanf("%d",&num[i]);
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=i+1;j<=n;j++)
+        {
+            for(int k=1;k<=n;k++)
+            {
+                if(num[k]==num[i]+num[j]&&flag[k]==0)
+                {
+                    t++;
+                    flag[k]=1;
+                }
+            }
+        }
+    }
+    printf("%d",t);
+    // system("pause");
+    return 0;
+}
--- ppt/Unit3/3-6-4.cpp
+++ ppt/Unit3/3-6-4.cpp
@@ -1,19 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     char type;
     float n;
-    scanf("%c%d",&type,&n);
-    switch(type)
+    scanf("%c%f", &type, &n);
+    switch (type)
     {
-        case 'P':
-        {
-            printf("%.2lf",0.7*n);
-        }
-        case 'G':
-        {
-            printf("")
-        }
+    case 'P':
+    {
+        printf("%.2lf", 0.7 * n);
+        break;
     }
+    case 'G':
+    {
+        printf("%.2lf", 0.8 * n);
+        break;
+    }
+    case 'S':
+    {
+        printf("%.2lf", 0.9 * n);
+        break;
+    }
+    default:
+    {
+        printf("%.2lf", 0.95 * n);
+        break;
+    }
+    }
+    system("pause");
+    return 0;
 }
