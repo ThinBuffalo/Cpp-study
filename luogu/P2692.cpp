b91d2564e0c8fba2291d1336a4cebbaa16501ecf
Thin Buffalo <mcluozc@163.com>
2021-01-06 17:39:55 +0800

    P2692

--- luogu/P2692.cpp
+++ luogu/P2692.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool a[5001][5001];
+bool a[5001][5001],h[5001];
 int n,m,b,g,t;
 int main()
 {
@@ -11,35 +11,43 @@ int main()
         scanf("%d%d",&x,&y);
         for(int j=x;j<=y;j++)
         {
-            for(int k=1;k<=m;k++)
-            {
-                a[k][j]=1;
-            }
+            // for(int k=1;k<=m;k++)
+            // {
+            //     a[k][j]=1;
+            // }
+            h[j]=1;
         }
     }
     for(int i=1;i<=g;i++)
     {
         int x,y;
         scanf("%d%d",&x,&y);
-        for(int j=x;j<=y;j++)
-        {
-            for(int k=1;k<=n;k++)
-            {
-                a[j][k]=1;
-            }
-        }
-    }
-    for(int i=1;i<=m;i++)
-    {
         for(int j=1;j<=n;j++)
         {
-            if(a[i][j]==1)
+            if(!h[j])
             {
-                t++;
+                for(int k=x;k<=y;k++)
+                {
+                    a[j][k]=1;
+                } 
+            }
+            
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(!h[i])
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(!a[i][j])
+                {
+                    t++;
+                }
             }
         }
     }
-    printf("%d",t);
+    printf("%d",n*m-t);
     system("pause");
     return 0;
 }
