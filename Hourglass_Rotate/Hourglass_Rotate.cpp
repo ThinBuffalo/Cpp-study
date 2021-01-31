ca74e2df4852f5690b65d7a40e382ed8173bf916
Thin_Buffalo <68691074+ThinBuffalo@users.noreply.github.com>
2021-01-31 11:54:25 +0800

    旋转沙漏

--- Hourglass_Rotate/Hourglass_Rotate.cpp
+++ Hourglass_Rotate/Hourglass_Rotate.cpp
@@ -1,23 +1,28 @@
 #include<bits/stdc++.h>
 #include<windows.h>
 using namespace std;
-const char a[]={'!','@','#','$','%','^','&','*','\000'};
+const char a[]= {'!','@','#','$','%','^','&','*'};
 int main()
 {
 	srand(time(0));
+	//得到语柄
+	HANDLE handle;
+	handle=GetStdHandle(STD_OUTPUT_HANDLE);
 	int length=strlen(a);
 	while(1)
 	{
 		//上下沙漏
-		for(int i=1;i<=15;i++)
+		for(int i=1; i<=15; i++)
 		{
-			for(int j=1;j<=15;j++)
+			char nowa=a[rand()%length];
+			SetConsoleTextAttribute(handle,rand()%15+1);//设置控制台颜色
+			for(int j=1; j<=15; j++)
 			{
 				if(i<=7)
 				{
 					if(j>=i&&j+i<15)
 					{
-						printf("%c",a[rand()%(length+1)]);
+						printf("%c",nowa);
 					}
 					else
 					{
@@ -28,7 +33,7 @@ int main()
 				{
 					if(j<i&&j+i>=15)
 					{
-						printf("%c",a[rand()%(length+1)]);
+						printf("%c",nowa);
 					}
 					else
 					{
@@ -37,19 +42,21 @@ int main()
 				}
 			}
 			printf("\n");
-		} 
+		}
 		Sleep(1000);
 		system("cls");
 		//左右沙漏
-		for(int i=1;i<=15;i++)
+		for(int i=1; i<=15; i++)
 		{
-			for(int j=1;j<=15;j++)
+			char nowa=a[rand()%(length+1)];
+			SetConsoleTextAttribute(handle,rand()%15+1);
+			for(int j=1; j<=15; j++)
 			{
 				if(i<=7)
 				{
-					if(j<=i||j+i>=15)
+					if(j<=i||j+i>15)
 					{
-						printf("%c",a[rand()%(length+1)]);
+						printf("%c",nowa);
 					}
 					else
 					{
@@ -60,7 +67,7 @@ int main()
 				{
 					if(j>i||j+i<=15)
 					{
-						printf("%c",a[rand()%(length+1)]);
+						printf("%c",nowa);
 					}
 					else
 					{
