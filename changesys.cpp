44d432404deb3515c23b84175cea2904a9cfd670
Thin Buffalo <mcluozc@163.com>
2021-03-02 14:03:33 +0800

    2021/3/2

--- changesys.cpp
+++ changesys.cpp
@@ -1,4 +1,3 @@
-//n进制转换10进制
 #include <bits/stdc++.h>
 using namespace std;
 //转char为int
@@ -38,9 +37,10 @@ float theSecondParty(int n, int k)
 int main()
 {
     char other[32];
-    int n, point, size, right = 2147483647;//int的最大值，为免48行使权为0与53行冲突(0.···)
+    int n1, n2, point, size, right = 2147483647; //int的最大值，为免48行使权为0与53行冲突(0.···)
     double ans = 0.0;
-    scanf("%d%s", &n, other);
+    scanf("%d%s%d", &n1, other, &n2); //第一个输代转换数字进制，第二个输代转换数字（数字中如有字母用大写），第三个输转换到的进制
+    //n进制转换10进制
     for (int i = 0; 1; i++)
     {
         if (other[i] == '.')
@@ -61,10 +61,31 @@ int main()
     {
         if (other[i] == '.')
             continue;
-        ans += number(other[i]) * theSecondParty(n, right);
+        ans += number(other[i]) * theSecondParty(n1, right);
         right--;
     }
-    printf("%lf\n", ans);
+    //10进制转n进制，暂只支持整数
+    int to[100] = {0.0};
+    int i;
+    int a = ans;
+    for (i = 1; a != 0; i++)
+    {
+        to[i] = a % n2;
+        a /= n2;
+    }
+    for (i = i - 1; i >= 1; i--)
+    {
+        if (n2 > 10.0)
+        {
+            printf("%c", (char)to[i] + 55);
+        }
+        else
+        {
+            printf("%d", to[i]);
+        }
+    }
+    printf("\n");
+    // printf("%lf\n", ans);
     system("pause");
     return 0;
 }
